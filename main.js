cdc5f3a70ac2a613b018c67ce903c6eb874df259
Ghandome <michaelogunleye407@gmail.com>
2025-01-06 19:30:22 +0100

    updated file

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ const paragraph = document.querySelectorAll(".paragraph");
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", ()=> {
+        const icon = button[i].querySelector("img");
         paragraph[i].classList.toggle("show");
-        button[i].querySelector(".btn").classList.toggle("icon-plus");
-        button[i].querySelector(".btn").classList.toggle("icon-minus");
+        icon.classList.toggle("icon-plus");
+        icon.classList.toggle("icon-minus");
+
+        if (icon.classList.contains("icon-plus")) {
+            icon.src = "./assets/images/icon-plus.svg";
+        } else {
+            icon.src = "./assets/images/icon-minus.svg";
+        }
     });
 }
